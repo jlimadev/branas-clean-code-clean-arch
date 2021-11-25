bfcb12c4d95c130bb525eb0f994474ffd55818d8
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-25 08:36:14 -0300

    refactor(cpf): apply clean code and refactor techniques

--- src/01-refactoring/validate-cpf.ts
+++ src/01-refactoring/validate-cpf.ts
@@ -1,48 +1,29 @@
-export function validate(str: string) {
-  if (str !== null) {
-    if (str !== undefined) {
-      if (str.length >= 11 || str.length <= 14) {
-        str = str.replace(".", "").replace(".", "").replace("-", "").replace(" ", "");
+const getDigitsFromCPF = (cpf: string): string => cpf.replace(/\D/g, "");
 
-        if (!str.split("").every((c) => c === str[0])) {
-          try {
-            let d1, d2;
-            let dg1, dg2, rest;
-            let digito;
-            let nDigResult;
-            d1 = d2 = 0;
-            dg1 = dg2 = rest = 0;
+const allDigitsAreEquals = (cpf: string): boolean => {
+  return cpf.split("").every((digit) => digit === cpf[0]);
+};
 
-            for (let nCount = 1; nCount < str.length - 1; nCount++) {
-              // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-              // 	return false;
-              // } else {
+const calculateVerifierDigit = (cpfDigits: string) => {
+  let multiplier = 2;
+  const digitsSum = Array.from(cpfDigits).reduceRight((acc, digit) => {
+    const sum = acc + multiplier * parseInt(digit, 10);
+    multiplier++;
+    return sum;
+  }, 0);
+  const rest = digitsSum % 11;
+  return rest < 2 ? 0 : 11 - rest;
+};
 
-              digito = parseInt(str.substring(nCount - 1, nCount));
-              d1 = d1 + (11 - nCount) * digito;
-
-              d2 = d2 + (12 - nCount) * digito;
-              // }
-            }
-
-            rest = d1 % 11;
-
-            dg1 = rest < 2 ? (dg1 = 0) : 11 - rest;
-            d2 += 2 * dg1;
-            rest = d2 % 11;
-            if (rest < 2) dg2 = 0;
-            else dg2 = 11 - rest;
-
-            let nDigVerific = str.substring(str.length - 2, str.length);
-            nDigResult = "" + dg1 + "" + dg2;
-            return nDigVerific == nDigResult;
-          } catch (e) {
-            console.error("Erro !" + e);
-
-            return false;
-          }
-        } else return false;
-      } else return false;
-    }
-  } else return false;
-}
+export const validate = (cpf: string): boolean => {
+  if (!cpf) return false;
+  const cpfDigits = getDigitsFromCPF(cpf);
+  if (cpfDigits.length !== 11) return false;
+  if (allDigitsAreEquals(cpfDigits)) return false;
+  const cpfWithNoVerifierDigits = cpfDigits.slice(0, -2);
+  const firstDigit = calculateVerifierDigit(cpfWithNoVerifierDigits);
+  const secondDigit = calculateVerifierDigit(`${cpfWithNoVerifierDigits}${firstDigit}`);
+  const currentVerifierDigits = cpfDigits.slice(-2);
+  const calculatedVerifierDigits = `${firstDigit}${secondDigit}`;
+  return currentVerifierDigits == calculatedVerifierDigits;
+};
