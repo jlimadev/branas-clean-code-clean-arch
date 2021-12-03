99ccd874f014134fc5bd1d837f6b17904fad7fb3
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-03 18:20:10 -0300

    test(coupon): split test in sessions

--- src/01-refactoring/coupon.spec.ts
+++ src/01-refactoring/coupon.spec.ts
@@ -1,24 +1,28 @@
 import { Coupon } from './coupon';
 
 describe('coupon', () => {
-  it('should calculate the discount', () => {
-    const coupon = new Coupon('20FF', 20);
-    expect(coupon.getDiscount(100)).toBe(20);
+  describe('getDiscount', () => {
+    it('should calculate the discount', () => {
+      const coupon = new Coupon('20FF', 20);
+      expect(coupon.getDiscount(100)).toBe(20);
+    });
   });
-  it('should return true when coupon is expired', () => {
-    const couponExpiredDate = new Date('2000-01-01');
-    const coupon = new Coupon('20FF', 20, couponExpiredDate);
-    expect(coupon.isExpired()).toBe(true);
-  });
-  it('should return false if coupon is not expired', () => {
-    const mockDate = new Date();
-    const couponValidDate = new Date(mockDate.getTime() + 1000 * 10);
-    jest.spyOn(global, 'Date').mockImplementation(() => mockDate as any);
-    const coupon = new Coupon('20FF', 20, couponValidDate);
-    expect(coupon.isExpired()).toBe(false);
-  });
-  it('should return isExpired as false when coupon has no expiration date', () => {
-    const coupon = new Coupon('20FF', 20);
-    expect(coupon.isExpired()).toBe(false);
+  describe('isExpired', () => {
+    it('should return true when coupon is expired', () => {
+      const couponExpiredDate = new Date('2000-01-01');
+      const coupon = new Coupon('20FF', 20, couponExpiredDate);
+      expect(coupon.isExpired()).toBe(true);
+    });
+    it('should return false if coupon is not expired', () => {
+      const mockDate = new Date();
+      const couponValidDate = new Date(mockDate.getTime() + 1000 * 10);
+      jest.spyOn(global, 'Date').mockImplementation(() => mockDate as any);
+      const coupon = new Coupon('20FF', 20, couponValidDate);
+      expect(coupon.isExpired()).toBe(false);
+    });
+    it('should return isExpired as false when coupon has no expiration date', () => {
+      const coupon = new Coupon('20FF', 20);
+      expect(coupon.isExpired()).toBe(false);
+    });
   });
 });
