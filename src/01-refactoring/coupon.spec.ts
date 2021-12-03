3b423e3165b5178b7c39d1c2186c12e400db7cb3
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-03 19:22:35 -0300

    feat(coupon): refactor variable name in coupon test

--- src/01-refactoring/coupon.spec.ts
+++ src/01-refactoring/coupon.spec.ts
@@ -15,7 +15,8 @@ describe('coupon', () => {
     });
     it('should return false if coupon is not expired', () => {
       const mockDate = new Date();
-      const couponValidDate = new Date(mockDate.getTime() + 1000 * 10);
+      const tenSeconds = 10 * 1000;
+      const couponValidDate = new Date(mockDate.getTime() + tenSeconds);
       jest.spyOn(global, 'Date').mockImplementation(() => mockDate as any);
       const coupon = new Coupon('20FF', 20, couponValidDate);
       expect(coupon.isExpired()).toBe(false);
