0c3f0c710d0124e4488e77a0c371d68f0b564775
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-03 18:18:17 -0300

    feat(coupon): validate if coupons isExpired

--- src/01-refactoring/coupon.ts
+++ src/01-refactoring/coupon.ts
@@ -1,7 +1,15 @@
 export class Coupon {
-  constructor(private readonly code: string, private readonly percentage: number) {}
+  constructor(
+    private readonly code: string,
+    private readonly percentage: number,
+    private readonly expirationDate?: Date,
+  ) {}
 
   getDiscount(amount: number): number {
     return (amount * this.percentage) / 100;
+  }
+
+  isExpired(today = new Date()) {
+    return this.expirationDate ? this.expirationDate < today : false;
   }
 }
