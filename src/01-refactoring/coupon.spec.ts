2c7ef750a55686745ef8be0548984799e7796d9a
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-04 09:36:47 -0300

    feat(coupon) refactor tests

--- src/01-refactoring/coupon.spec.ts
+++ src/01-refactoring/coupon.spec.ts
@@ -11,19 +11,16 @@ describe('coupon', () => {
     it('should return true when coupon is expired', () => {
       const couponExpiredDate = new Date('2000-01-01');
       const coupon = new Coupon('20FF', 20, couponExpiredDate);
-      expect(coupon.isExpired()).toBe(true);
+      expect(coupon.isExpired(new Date())).toBeTruthy();
     });
     it('should return false if coupon is not expired', () => {
-      const mockDate = new Date();
-      const tenSeconds = 10 * 1000;
-      const couponValidDate = new Date(mockDate.getTime() + tenSeconds);
-      jest.spyOn(global, 'Date').mockImplementation(() => mockDate as any);
-      const coupon = new Coupon('20FF', 20, couponValidDate);
-      expect(coupon.isExpired()).toBe(false);
+      const expirationDate = new Date('2021-01-01');
+      const coupon = new Coupon('20FF', 20, expirationDate);
+      expect(coupon.isExpired(new Date('2020-01-01'))).toBeFalsy();
     });
     it('should return isExpired as false when coupon has no expiration date', () => {
       const coupon = new Coupon('20FF', 20);
-      expect(coupon.isExpired()).toBe(false);
+      expect(coupon.isExpired()).toBeFalsy();
     });
   });
 });
