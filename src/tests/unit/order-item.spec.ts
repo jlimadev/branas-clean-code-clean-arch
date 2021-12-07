5bbaab2a807551a9e29402987c78c39eb36a6c41
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-07 17:04:08 -0300

    test(order-item): ensure it returns the order item

---
+++ src/tests/unit/order-item.spec.ts
@@ -0,0 +1,8 @@
+import { OrderItem } from '../../domain/entities/order-item';
+
+describe('order-item', () => {
+  it('should create an order item', () => {
+    const orderItem = new OrderItem(1, 1000, 10);
+    expect(orderItem.getTotal()).toBe(10000);
+  });
+});
