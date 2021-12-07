599a228896d9926b54bc573009012056b97e20c7
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-07 16:53:31 -0300

    feat(order): ensure it returns the freight

--- src/domain/entities/order.ts
+++ src/domain/entities/order.ts
@@ -2,33 +2,45 @@ import { CPF } from './cpf';
 import { OrderItem } from './order-item';
 import { Item } from './item';
 import { Coupon } from './coupon';
+import { DefaultFreightCalculator } from './default-freight-calculator';
+import { FreightCalculator } from '../interfaces/freight-calculator';
 
 export class Order {
   cpf: CPF;
-
   coupon: Coupon | undefined;
+  private orderItems: OrderItem[];
+  private freight: number;
 
-  orderItems: OrderItem[];
-
-  constructor(readonly customerCpf: string) {
+  constructor(
+    readonly customerCpf: string,
+    readonly date: Date = new Date(),
+    readonly freightCalculator: FreightCalculator = new DefaultFreightCalculator(),
+  ) {
     this.cpf = new CPF(customerCpf);
     this.orderItems = [];
+    this.freight = 0;
   }
 
   addCoupon(coupon: Coupon) {
-    if (coupon.isExpired()) return;
+    if (coupon.isExpired(this.date)) return;
     this.coupon = coupon;
   }
 
   addItem(item: Item, quantity: number) {
     this.orderItems.push(new OrderItem(item.id, item.price, quantity));
+    this.freight = this.freightCalculator.calculate(item) * quantity;
+  }
+
+  getFreight() {
+    return this.freight;
   }
 
   getTotal() {
-    const total = this.orderItems.reduce((acc, item) => acc + item.getTotal(), 0);
+    let total = this.orderItems.reduce((acc, item) => acc + item.getTotal(), 0);
     if (this.coupon) {
-      return total - this.coupon.getDiscount(total);
+      total -= this.coupon.calculateDiscount(total, this.date);
     }
+    total += this.freight;
     return total;
   }
 }
