4a0f40f043dc9c9d97ea1b37a3841a3906558576
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-08 16:59:28 -0300

    test(place-order): ensure it will return an order with freight

--- tests/integration/place-order.spec.ts
+++ tests/integration/place-order.spec.ts
@@ -22,4 +22,21 @@ describe('place order', () => {
     const output = await placeOrder.invoke(input);
     expect(output.total).toBe(88);
   });
+  it('should place an order with freight', async () => {
+    const itemRepository = new ItemRepositoryInMemory();
+    const couponRepository = new CouponRepositoryInMemory();
+    const orderRepository = new OrderRepositoryInMemory();
+    const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository);
+    const input = {
+      cpf: '935.411.347-80',
+      orderItems: [
+        { idItem: 4, quantity: 1 },
+        { idItem: 5, quantity: 1 },
+        { idItem: 6, quantity: 3 },
+      ],
+      date: new Date(),
+    };
+    const output = await placeOrder.invoke(input);
+    expect(output.total).toBe(6350);
+  });
 });
