8f0c9bab2740860fee2c10cb12b8187d705be2fb
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-08 16:58:50 -0300

    test(place-order): ensure it will return an order with discount coupon

---
+++ tests/integration/place-order.spec.ts
@@ -0,0 +1,25 @@
+import { PlaceOrder } from '../../src/application/usecases/place-order/place-order';
+import ItemRepositoryInMemory from '../../src/infra/repository/memory/item-repository-in-memory';
+import CouponRepositoryInMemory from '../../src/infra/repository/memory/coupon-repository-in-memory';
+import OrderRepositoryInMemory from '../../src/infra/repository/memory/order-repository-in-memory';
+
+describe('place order', () => {
+  it('should place an order with discount', async () => {
+    const itemRepository = new ItemRepositoryInMemory();
+    const couponRepository = new CouponRepositoryInMemory();
+    const orderRepository = new OrderRepositoryInMemory();
+    const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository);
+    const input = {
+      cpf: '935.411.347-80',
+      orderItems: [
+        { idItem: 1, quantity: 1 },
+        { idItem: 2, quantity: 1 },
+        { idItem: 3, quantity: 3 },
+      ],
+      date: new Date(),
+      coupon: '20OFF',
+    };
+    const output = await placeOrder.invoke(input);
+    expect(output.total).toBe(88);
+  });
+});
