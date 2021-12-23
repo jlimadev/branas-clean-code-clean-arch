b5b6c46ee7be423f62cbd315995a10a5d3abb58e
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-23 15:06:58 -0300

    test(validate-coupon): add makesut

--- tests/integration/validate-coupon.spec.ts
+++ tests/integration/validate-coupon.spec.ts
@@ -2,28 +2,28 @@ import PgPromiseConnectionAdapter from '../../src/infra/database/pg-promise-conn
 import ValidateCoupon from '../../src/application/usecases/validate-coupon/validate-coupon';
 import CouponRepositoryDatabase from '../../src/infra/repository/database/coupon-repository-database';
 
+const makeSut = (): ValidateCoupon => {
+  const connection = PgPromiseConnectionAdapter.getInstance();
+  const couponRepository = new CouponRepositoryDatabase(connection);
+  return new ValidateCoupon(couponRepository);
+};
+
 describe('validate-coupon', () => {
   it('should validate the coupon', async () => {
-    const connection = PgPromiseConnectionAdapter.getInstance();
-    const couponRepository = new CouponRepositoryDatabase(connection);
-    const validateCoupon = new ValidateCoupon(couponRepository);
+    const sut = makeSut();
     const couponCode = '20OFF';
-    const isValid = await validateCoupon.invoke(couponCode);
+    const isValid = await sut.invoke(couponCode);
     expect(isValid).toBe(true);
   });
   it('should should throw if the coupon does not exists', async () => {
-    const connection = PgPromiseConnectionAdapter.getInstance();
-    const couponRepository = new CouponRepositoryDatabase(connection);
-    const validateCoupon = new ValidateCoupon(couponRepository);
+    const sut = makeSut();
     const couponCode = 'ANY-INVALID-COUPON';
-    await expect(() => validateCoupon.invoke(couponCode)).rejects.toThrow();
+    await expect(() => sut.invoke(couponCode)).rejects.toThrow();
   });
   it('should return false if coupon is expired', async () => {
-    const connection = PgPromiseConnectionAdapter.getInstance();
-    const couponRepository = new CouponRepositoryDatabase(connection);
-    const validateCoupon = new ValidateCoupon(couponRepository);
+    const sut = makeSut();
     const couponCode = '20OFF_EXPIRED';
-    const isValid = await validateCoupon.invoke(couponCode);
+    const isValid = await sut.invoke(couponCode);
     expect(isValid).toBe(false);
   });
 });
